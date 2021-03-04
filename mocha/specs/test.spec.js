02a5f9a27c8b1bf884bff9d7e900eccfc8ae425a
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-03-04 19:06:44 +0100

    Defined test cases in the mocha folder for the epam.com careers page.

---
+++ mocha/specs/test.spec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('Search for job', () => {
+   describe('Careers page', () => {
+       it('should be opened', () => {
+
+       });
+   }) ;
+
+   describe('Search form', () => {
+       it('should be displayed', () => {
+
+       });
+
+       describe('Location filter box', () => {
+           it('should provide a way to filter to a specific location', () => {
+
+           });
+       });
+
+       describe('Department filter box', () => {
+           it('should provide a way to filter to a specific department', () => {
+
+           });
+       });
+
+       describe('Searching', () => {
+          it('should have proper job found', () => {
+
+          });
+
+          it('should have job with proper department', () => {
+
+          });
+
+          it('should have job with proper location', () => {
+
+          });
+
+          it('should have apply button for job', () => {
+
+          });
+
+          describe('Applying to position', () => {
+              it('should have proper position name in the description', () => {
+
+              });
+
+              it('should have proper location in the description', () => {
+
+              });
+          })
+       });
+   });
+});
