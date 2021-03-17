642ba73935e351ea77a94dae5bcc50a69fbc7598
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-03-17 12:14:41 +0100

    Added comments for the complex functions.

--- pageObjects/careerPage.js
+++ pageObjects/careerPage.js
@@ -12,7 +12,7 @@ class CareerPage {
         this.getCityOfLocation = city => element(by.cssContainingText('#select2-new_form_job_search_1445745853_copy-location-results > li > ul > li', city));
 
         this.departmentSelect = this.searchForm.element(by.css('div:nth-child(3) > div > div.selected-params'));
-        this.getDepartmentCheckbox = department => element(by.cssContainingText('div.multi-select-dropdown > ul:nth-child(2) > li > label > span', department))
+        this.getDepartmentCheckbox = department => element(by.cssContainingText('div.multi-select-dropdown > ul:nth-child(2) > li > label', department))
         this.selectedDepartments = element.all(by.css('li.filter-tag'));
 
         this.searchResultItems = element.all(by.css('.search-result__list'));
@@ -22,19 +22,51 @@ class CareerPage {
 
         this.waitForPositionVisibility = item => browser.wait(ec.visibilityOf(this.nameOfPosition(item)), GLOBAL_TIMEOUT);
 
+        /**
+         *
+         * @param name: The position that we search for.
+         * @returns {*|ElementFinder}
+         * Finds the position with the specified name, from the list of positions.
+         */
         this.getResultByPosition = name => this.searchResultItems.filter(item => {
             this.waitForPositionVisibility(item);
             return this.nameOfPosition(item).getText().then(position => position.trim().includes(name));
         }).first();
 
         this.jobDescription = element(by.css('.recruiting-page__top-description strong'));
+        this.acceptCookiesButton = element(by.css('.cookie-disclaimer__button'));
     }
 
+    /**
+     * Waits 0.5 sec for the cookie bar and if it's append accept it.
+     * If the cookie bar not appends continues the test load.
+     * @returns {PromiseLike<void>}
+     */
+    acceptCookies() {
+        const cookieButton = this.acceptCookiesButton;
+        return browser.wait(ec.elementToBeClickable(cookieButton), 500).then(() => {
+            cookieButton.click();
+        }, e => {});
+    }
+
+    /**
+     * @returns {PromiseLike<void>}
+     * Load the webpage and waits for the logo to be clickable.
+     * If the page is loaded, checks the cookie bar and accept is if necessary.
+     */
     load() {
         browser.get('https://www.epam.com/careers');
-        return browser.wait(ec.elementToBeClickable(this.logo), GLOBAL_TIMEOUT);
+        browser.wait(ec.elementToBeClickable(this.logo), GLOBAL_TIMEOUT);
+        return this.acceptCookies();
     }
 
+    /**
+     * @param county: The name of the country we want to search for.
+     * @param city: The name of the city we want to search for.
+     * @returns {promise.Promise<void>}
+     * This function made for the test automation of search bar in the careers page.
+     * If the location filter box is not open opens it and click the specified country and city.
+     */
     selectCityInCountry(county, city) {
         const countryOption = this.getCountryOfLocation(county);
         countryOption.isDisplayed().then(displayed => {
@@ -51,8 +83,14 @@ class CareerPage {
         return cityOption.click();
     }
 
+    /**
+     *
+     * @param department
+     * @returns {promise.Promise<void>}
+     * Checks if the specified department is displayed.
+     * If it's displayed click it. If not, opens the toggle and click it.
+     */
     toggleDepartment(department) {
-        //click all and assert
         const departmentCheckbox = this.getDepartmentCheckbox(department);
         departmentCheckbox.isDisplayed().then(displayed => {
             if (!displayed) {
@@ -67,6 +105,10 @@ class CareerPage {
         return this.selectedLocation.getText();
     }
 
+    /**
+     * @returns {promise.Promise<boolean>}
+     * Clicks the search button and checks that is there any result.
+     */
     search() {
         this.searchButton.click();
         return browser.wait(() => {
@@ -74,6 +116,12 @@ class CareerPage {
         }, GLOBAL_TIMEOUT);
     }
 
+    /**
+     *
+     * @param position: The position that we want to apply for.
+     * @returns {WebElementPromise}
+     * Apply for the specified position and checks that the description is correct.
+     */
     applyForPosition(position) {
         this.applyButtonOfPosition(position).click();
         return browser.wait(ec.visibilityOf(this.jobDescription), GLOBAL_TIMEOUT);
