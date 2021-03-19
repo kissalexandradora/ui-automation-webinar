5e7bdce65fbb2cb404e89a1c289c1c891c1d3e49
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-03-19 09:32:01 +0100

    Updated all regExp in the cucumber step definition file.

--- cucumber/step_definitions/careerPage.js
+++ cucumber/step_definitions/careerPage.js
@@ -11,19 +11,19 @@ defineSupportCode(({ Given, When, Then, setDefaultTimeout }) => {
         return careerPage.load();
     });
 
-    When(/(.+), (.+) is selected in the location filter box/, (city, country) => {
+    When(/^(.+), (.+) is selected in the location filter box$/, (city, country) => {
         return careerPage.selectCityInCountry(country, city);
     });
 
-    When(/(.+) is selected in the department filter box/, department => {
+    When(/^(.+) is selected in the department filter box$/, department => {
         return careerPage.toggleDepartment(department);
     });
 
-    When(/the search button is clicked/, () => {
+    When(/^the search button is clicked$/, () => {
         return careerPage.search();
     });
 
-    Then(/the logo should be visible/, () => {
+    Then(/^the logo should be visible$/, () => {
         return expect(careerPage.logo.isDisplayed()).to.eventually.be.true;
     });
 
@@ -31,24 +31,24 @@ defineSupportCode(({ Given, When, Then, setDefaultTimeout }) => {
         return expect(careerPage.cookieBanner.isVisible()).to.eventually.be.equal(visibility === "visible");
     });
 
-    Then(/the search form should be visible/, () => {
+    Then(/^the search form should be visible$/, () => {
         return expect(careerPage.searchForm.isDisplayed()).to.eventually.be.true;
     });
 
-    Then(/(.+) should be selected in the location filter box/, city => {
+    Then(/^(.+) should be selected in the location filter box$/, city => {
         return expect(careerPage.getSelectedCity()).to.eventually.equal(city);
     });
 
-    Then(/(.+) should be selected in the department filter box/, department => {
+    Then(/^(.+) should be selected in the department filter box$/, department => {
         return expect(careerPage.selectedDepartments.getText()).to.eventually.contain(department.toUpperCase());
     })
 
-    Then(/there should be a job offer for (.+) position/, positionName => {
+    Then(/^there should be a job offer for (.+) position$/, positionName => {
         return expect(careerPage.nameOfPosition(careerPage.getResultByPosition(positionName))
             .getText()).to.eventually.contain(positionName);
     })
 
-    Then(/the location of the <PositionName> position should be <City>, <Country>/, () => {
+    Then(/^the location of the <PositionName> position should be <City>, <Country>$/, () => {
 
     });
 });
