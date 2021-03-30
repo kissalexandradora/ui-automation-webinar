1821f84f0f856bfb96de361142e71b1fa05aade4
Kristof Keseru <kristof_keseru@epam.com>
2021-03-30 16:52:21 +0200

    Corrected typo in step definitions.

--- cucumber/step_definitions/careerPage.js
+++ cucumber/step_definitions/careerPage.js
@@ -10,11 +10,11 @@ Given(/^the career page is opened$/, () => {
     return careerPage.load();
 });
 
-When(/^(.+), (.+) is selected in the location filter box$/, (city, country) => {
+When(/^the (.+), (.+) is selected in the location filter box$/, (city, country) => {
     return careerPage.selectCityInCountry(country, city);
 });
 
-When(/^(.+) is selected in the department filter box$/, department => {
+When(/^the (.+) is selected in the department filter box$/, department => {
     return careerPage.toggleDepartment(department);
 });
 
@@ -40,11 +40,11 @@ Then(/^the search form should be visible$/, () => {
     return expect(careerPage.searchForm.isDisplayed()).to.eventually.be.true;
 });
 
-Then(/^(.+) should be selected in the location filter box$/, city => {
+Then(/^the (.+) should be selected in the location filter box$/, city => {
     return expect(careerPage.getSelectedCity()).to.eventually.equal(city);
 });
 
-Then(/^(.+) should be selected in the department filter box$/, department => {
+Then(/^the (.+) should be selected in the department filter box$/, department => {
     return expect(careerPage.selectedDepartments.getText()).to.eventually.contain(department.toUpperCase());
 });
 
