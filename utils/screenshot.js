b5c2d8e71e9660377c24e464df0cc37afbc5a2a3
Kristof Keseru <kristof_keseru@epam.com>
2021-04-19 19:38:47 +0200

    Moved over the screenshot function to the screenshot.js file.

--- utils/screenshot.js
+++ utils/screenshot.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
+//const dayjs = require("dayjs");
+const { toCamelCase }  = require("./stringFormatter");
+const { getFormattedTime } = require("./date");
 
-exports.writeScreenShot = (data, filename) => {
+
+const writeScreenShot = (data, filename) => {
     const stream = fs.createWriteStream(filename);
     stream.write(new Buffer(data, 'base64'));
     stream.end();
+}
+exports.takeScreenshot = async scenario => {
+    const screenshot = await browser.takeScreenshot();
+    await writeScreenShot(screenshot, `./reports/screenshots/${toCamelCase(scenario.pickle.name)}-${getFormattedTime()}.png`);
 }
