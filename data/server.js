c884d504ed725a49405b2b1e22237536642dfcc0
Kristof Keseru <kristof_keseru@epam.com>
2021-05-28 16:04:52 +0200

    Added .env to the .gitignore file.

--- data/server.js
+++ data/server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+/**
+ * Gives access to the .env file.
+ */
+require('dotenv/config');
 
 /**
  * The app const gives the ability to create routes
@@ -12,6 +19,19 @@ app.get('/', (req, res) => {
     res.send('SUCCESS');
 });
 
+/**
+ * Connect the server to the MongoDB Atlas database.
+ */
+mongoose.connect(
+    /**
+     * Use the database connection credentials from the .env file.
+     * The useNewUrlParser is needed to avoid warnings.
+     */
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => {
+    console.log('Connected to database.');
+});
 
 /**
  * Specifies the port.
