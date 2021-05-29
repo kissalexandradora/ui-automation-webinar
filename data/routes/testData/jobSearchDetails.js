74f568737563859ad69c1c30f1498623650f6985
Kristof Keseru <kristof_keseru@epam.com>
2021-05-29 10:50:39 +0200

    Implemented the Job Search Details route.

--- data/routes/testData/jobSearchDetails.js
+++ data/routes/testData/jobSearchDetails.js
@@ -1,9 +1,44 @@
 const express = require('express');
-
 const router = express.Router();
+const JobSearchDetail = require('../../models/testData/JobSearchDetails');
 
-router.get('/', (req, res) => {
-    res.send('Test data');
+/**
+ * Return all the jobSearchDetails.
+ */
+router.get('/', async (req, res) => {
+    const jobSearchDetails = await JobSearchDetail.find();
+    res.json(jobSearchDetails);
 });
+
+/**
+ * Return a specific jobSearchDetail.
+ */
+router.get('/:jobSearchDetailId', async (req, res) => {
+    const jobSearchDetail = await JobSearchDetail.findById(req.params.jobSearchDetailId);
+    res.json(jobSearchDetail);
+});
+
+/**
+ * Submit a jobSearchDetail.
+ */
+router.post('/', async (req, res) => {
+    const jobSearchDetail = new JobSearchDetail({
+        country: req.body.country,
+        city: req.body.city,
+        department: req.body.department,
+        positionName: req.body.positionName
+    });
+
+    const savedJobSearchDetail = await jobSearchDetail.save();
+    res.json(savedJobSearchDetail);
+});
+
+/**
+ * Delete a specific jobSearchDetail
+ */
+router.delete('/:jobSearchDetailId', async (req, res) => {
+    const removedPost = await JobSearchDetail.remove({ _id:req.params.jobSearchDetailId });
+    res.json(removedPost);
+})
 
 module.exports = router;
